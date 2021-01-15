6ad56f49e415a1e21c4b580ba54f8a89d6785f0c
drozen <drozen@campus.technion.ac.il>
2021-01-15 12:12:28 +0200

    IDT cut

--- CostSensitiveID3.py
+++ CostSensitiveID3.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 
 from sklearn.model_selection import KFold
 
-from ID3 import load_tables, select_feature
+from ID3 import load_tables, select_feature, TDIDT_CUT
 from ID3 import create_Features
 from ID3 import TDIDT
 from ID3 import IDT_basic_classifier
@@ -23,6 +23,25 @@ def majority_class(examples):
         return 'B'
 
 
+def evaluate(correct, assume):
+    if correct == assume:
+        return 0
+    if correct == 'B' and assume == 'M':
+        return 1
+    return 10
+
+
+def classify(tree, example):
+    if tree.children == None:
+        return tree.c
+    num_of_feature = tree.feature
+    diverge_val = tree.diverge_val
+    if (example[num_of_feature] >= diverge_val):
+        return classify(tree.children[0], example)
+    else:
+        return classify(tree.children[1], example)
+
+
 def prune(T, V):
     if (T.children == None):
         return T
@@ -37,21 +56,21 @@ def prune(T, V):
     child2 = prune(T.children[1], Echild2)
     T.children = [child1, child2]
     # T= best tree until yet
-    classifier_Tree = IDT_basic_classifier(None, V, None, T)
-    err_no_prune = classifier_Tree.predict_IDT_loss(False)
-    CutT = deepcopy(T)
-    CutT.children = []
-    CutT.feature = None
-    CutT.diverge_val = None
-    classifier_Cut_Tree = IDT_basic_classifier(None, V, None, CutT)
-    err_prune = classifier_Tree.predict_IDT_loss(False)
+    err_prune = 0
+    err_no_prune = 0
+    for example in V:
+        err_prune += evaluate(example[0], T.c)
+        res_no_prune = classify(T, example)
+        err_no_prune += evaluate(example[0], res_no_prune)
     if err_prune < err_no_prune:
-        return CutT
+        T.children = None
+        T.feature = None
+        T.diverge_val = None
     return T
 
 
-def split_train_tabels(train_tabels):
-    kf = KFold(2, True, 318965365)  # TODO: change to my ID
+def split_train_tabels(train_tabels,num=2):
+    kf = KFold(num, True, 123456789)  # TODO: change to my ID
     for train_index, test_index in kf.split(train_tabels):  # loop 5 times
         trainE = []
         testE = []
@@ -65,14 +84,51 @@ def split_train_tabels(train_tabels):
 if __name__ == '__main__':
     train_tables = load_tables("train.csv")
     test_tables = load_tables("test.csv")
-    c = majority_class(train_tables)
-    F = create_Features(len(train_tables[0]))
 
     V_train, V_test = split_train_tabels(train_tables)
+    c = majority_class(V_train)
+    F = create_Features(len(V_train[0]))
     T = TDIDT(V_train, F, c, select_feature)
 
-    best_tree = prune(T, V_test )
-
-    basic_classifier = IDT_basic_classifier(train_tables, test_tables, None, best_tree)
+    basic_classifier = IDT_basic_classifier(None, test_tables, None, T)
     x = basic_classifier.predict_IDT_loss(False)
-    print(x)
+    #print(x)
+
+    best_tree = prune(T, V_test)
+
+    basic_classifier1 = IDT_basic_classifier(None, test_tables, None, best_tree)
+    loss = basic_classifier1.predict_IDT_loss(False)
+    print(loss)
+
+    """
+    ניסיון לכוונן ערכים:
+    """
+    # minloss=1
+    # minloss_index=-1
+    # min_lossM=-1
+    # V_train, V_test = split_train_tabels(train_tables)
+    # F = create_Features(len(V_train[0]))
+    # c = majority_class(V_train)
+    # for M in range (1,40):
+    #     cut_tree = TDIDT_CUT(V_train, F, c, select_feature, M)
+    #     print(M, " mParemeter")
+    #     for i in range (2,40):
+    #         print(i)
+    #         V_train, V_test = split_train_tabels(train_tables, i)
+    #         basic_classifier = IDT_basic_classifier(None, test_tables, None, cut_tree)
+    #         x = basic_classifier.predict_IDT_loss(False)
+    #         print(x)
+    #         best_tree = prune(cut_tree, V_test)
+    #         basic_classifier1 = IDT_basic_classifier(None, test_tables, None, best_tree)
+    #         x = basic_classifier1.predict_IDT_loss(False)
+    #         print(x)
+    #         if(x<minloss):
+    #             minloss=x
+    #             minloss_index=i
+    #             min_lossM=M
+    #
+    # print(minloss_index,minloss,min_lossM)
+
+    #M=2 0.012389380530973451 k=2
+    #M=12 0.001769911504424779 k=3
+    #K=4 0.004424778761061947 no early cut
